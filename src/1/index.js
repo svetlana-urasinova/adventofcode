bcea6a983df1598b8541e9d5697b2c5130dbeca4
svetlana-urasinova <urasinova.work@gmail.com>
2022-11-03 23:49:12 +0100

    Solve 1-2 and add tests

--- src/1/index.js
+++ src/1/index.js
@@ -1,4 +1,4 @@
-import { input } from './part1-input.js';
+import { input } from './input.js';
 export function main() {
   const part1Answer = part1();
   const part2Answer = part2();
@@ -13,9 +13,6 @@ function part1() {
   return input.trim().split("\n").reduce((sum, massStr) => {
     if (!massStr) { return sum }
 
-    // console.log(sum + BigInt(Math.floor(Number(str) / 3)));
-    // return sum + BigInt(Math.floor(Number(str) / 3) - 2)
-    console.log(`${massStr} -> ${getFuelAmount(massStr)}`);
     return sum + getFuelAmount(Number(massStr))
   }, 0).toString();
 }
@@ -27,10 +24,20 @@ function part2() {
     // console.log(sum + BigInt(Math.floor(Number(str) / 3)));
     // return sum + BigInt(Math.floor(Number(str) / 3) - 2)
     console.log(`${massStr} -> ${getFuelAmount(massStr)}`);
-    return sum + getFuelAmount(Number(massStr))
+    return sum + getUpdatedFuelAmount(Number(massStr))
   }, 0).toString();
 }
 
-function getFuelAmount(mass) {
+export function getFuelAmount(mass) {
   return Math.floor(mass / 3) - 2;
 }
+
+export function getUpdatedFuelAmount(mass, totalMass = 0) {
+  while (mass > 5) {
+    mass = getFuelAmount(mass);
+    totalMass += mass;
+  }
+
+  return totalMass;
+}
+
