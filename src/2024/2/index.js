625f1e4cd1be4ad2fc083fe5a854612143826c0a
svetlana-urasinova <urasinova.work@gmail.com>
2024-12-02 15:25:17 +0100

    Solve part 1

--- src/2024/2/index.js
+++ src/2024/2/index.js
@@ -1,4 +1,7 @@
-import { input } from './input-example.js';
+import { input } from './input.js';
+
+const PART_1_MIN = 1;
+const PART_1_MAX = 3;
 
 export function main() {
   const data = getInputData(input);
@@ -7,13 +10,13 @@ export function main() {
   const part2Answer = part2(data);
 
   return `
-        <p><span class="answer">${part1Answer}</span></p>        
+        <p>Total amount of safe reports is <span class="answer">${part1Answer}</span>.</p>        
         <p><span class="answer">${part2Answer}</span></p>        
     `;
 }
 
 export function part1(data) {
-  return '';
+  return data.reduce((total, report) => (isSafe(report) ? ++total : total), 0);
 }
 
 export function part2(data) {
@@ -21,5 +24,19 @@ export function part2(data) {
 }
 
 export function getInputData(data) {
-  return data;
+  return data.split('\n').map(line => line.split(/\s+/).map(el => +el));
+}
+
+function isSafe(report) {
+  const sign = Math.sign(report[1] - report[0]);
+
+  for (let i = 1; i < report.length; i++) {
+    const diff = report[i] - report[i - 1];
+
+    if (Math.sign(diff) !== sign || Math.abs(diff) < PART_1_MIN || Math.abs(diff) > PART_1_MAX) {
+      return false;
+    }
+  }
+
+  return true;
 }
--- src/2024/2/index.test.js
+++ src/2024/2/index.test.js
@@ -1,11 +1,11 @@
 import { part1, part2, getInputData } from './index.js';
 import { input } from './input-example.js';
 
-describe('2024-%DAY%', () => {
+describe('2024-2', () => {
   const data = getInputData(input);
 
-  test('part 1: %TEST_NAME%', () => {
-    // expect(part1(data)).toBe();
+  test('part 1: number of safe reports', () => {
+    expect(part1(data)).toBe(2);
   });
 
   test('part 2: %TEST_NAME%', () => {
