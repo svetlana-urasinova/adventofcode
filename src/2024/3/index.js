cb4efec89acae3246bd6372853459bdf414ed591
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2024-12-03 13:25:34 +0100

    Solve part 2

--- src/2024/3/index.js
+++ src/2024/3/index.js
@@ -1,27 +1,147 @@
 import { input } from './input.js';
 
-export function main() {
-  const data = getInputData(input);
+const INSTRUCTIONS = {
+  multiply: {
+    name: 'mul',
+    argsNumber: 2,
+  },
+  enabled: {
+    name: 'do',
+  },
+  disabled: {
+    name: "don't",
+  },
+};
 
-  const part1Answer = part1(data);
-  const part2Answer = part2(data);
+const ARGS_START_CHAR = '(';
+const ARGS_END_CHAR = ')';
+const ARGS_SEPARATOR_CHAR = ',';
+
+export function main() {
+  const part1Answer = part1(input);
+  const part2Answer = part2(input);
 
   return `
         <p><span class="answer">Sum of all multiplications is ${part1Answer}</span>.</p>        
-        <p><span class="answer">${part2Answer}</span></p>        
+        <p><span class="answer">Sum of only enabled multiplications is ${part2Answer}</span>.</p>        
     `;
 }
 
-export function part1(data) {
-  return data.reduce((sum, pair) => sum + pair[0] * pair[1], 0);
+export function part1(input) {
+  const data = parseMultiplyInstructions(input);
+
+  return calculateSumOfMultiplications(data);
 }
 
-export function part2(data) {
-  return '';
+export function part2(input) {
+  const data = parseMultiplyInstructions(input, true);
+
+  return calculateSumOfMultiplications(data);
 }
 
-export function getInputData(data) {
-  const instructions = data.match(/mul\(\d+,\d+\)/g);
+function parseMultiplyInstructions(input, enabledOnly = false) {
+  const result = [];
 
-  return instructions.map(instruction => instruction.match(/\d+/g).map(el => +el));
+  let enabled = true;
+
+  for (let i = 0; i < input.length; i++) {
+    if (enabledOnly) {
+      const disabledInstructionResult = parseInstruction(INSTRUCTIONS.disabled, input, i);
+
+      if (disabledInstructionResult !== null) {
+        enabled = false;
+
+        i += disabledInstructionResult.instruction.length - 1;
+
+        continue;
+      }
+
+      const enabledInstructionResult = parseInstruction(INSTRUCTIONS.enabled, input, i);
+
+      if (enabledInstructionResult !== null) {
+        enabled = true;
+
+        i += enabledInstructionResult.instruction.length - 1;
+
+        continue;
+      }
+
+      if (!enabled) {
+        continue;
+      }
+    }
+
+    const multiplyInstructionResult = parseInstruction(INSTRUCTIONS.multiply, input, i);
+
+    if (multiplyInstructionResult !== null) {
+      result.push(multiplyInstructionResult.args);
+
+      i += multiplyInstructionResult.instruction.length - 1;
+    }
+  }
+
+  return result;
+}
+
+function parseInstruction(instruction, str, start) {
+  const { name, argsNumber } = instruction;
+
+  if (str.slice(start, start + name.length) !== name) {
+    return null;
+  }
+
+  const parsedArgs = parseInstructionArgs(str, start + name.length, argsNumber);
+
+  if (!parsedArgs) {
+    return null;
+  }
+
+  return { instruction: str.slice(start, parsedArgs.end), args: parsedArgs.args };
+}
+
+function parseInstructionArgs(str, start, argsNumber) {
+  const args = [];
+
+  let counter = start;
+
+  if (str[counter] !== ARGS_START_CHAR) {
+    return null;
+  }
+
+  counter++;
+
+  if (!argsNumber) {
+    return str[counter] === ARGS_END_CHAR ? { args, end: counter } : null;
+  }
+
+  for (let i = 0; i < argsNumber; i++) {
+    let currentArg = '';
+
+    while (/\d/.test(str[counter])) {
+      currentArg += str[counter];
+      counter++;
+    }
+
+    const isLastIteration = i + 1 === argsNumber;
+
+    if (
+      currentArg !== '' &&
+      ((isLastIteration && str[counter] === ARGS_END_CHAR) ||
+        (!isLastIteration && str[counter] === ARGS_SEPARATOR_CHAR))
+    ) {
+      args.push(+currentArg);
+    } else {
+      return null;
+    }
+
+    if (!isLastIteration) {
+      counter++;
+    }
+  }
+
+  return { args, end: counter };
+}
+
+function calculateSumOfMultiplications(pairs) {
+  return pairs.reduce((sum, pair) => sum + pair[0] * pair[1], 0);
 }
--- src/2024/3/index.test.js
+++ src/2024/3/index.test.js
@@ -1,14 +1,13 @@
 import { part1, part2, getInputData } from './index.js';
-import { input } from './input-example.js';
+import { input as input1 } from './input-example.js';
+import { input as input2 } from './input-example2.js';
 
 describe('2024-3', () => {
-  const data = getInputData(input);
-
   test('part 1: should calculate the sum of all multiplications', () => {
-    expect(part1(data)).toBe(161);
+    expect(part1(input1)).toBe(161);
   });
 
-  test('part 2: %TEST_NAME%', () => {
-    // expect(part2(data)).toBe();
+  test('part 2: should calculate the sum of only enabled multiplications', () => {
+    expect(part2(input2)).toBe(48);
   });
 });
