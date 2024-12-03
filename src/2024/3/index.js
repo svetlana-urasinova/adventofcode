55a4b40aa80172bd9b6a24794bf58aa1509d6358
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2024-12-03 11:35:26 +0100

    Solve part 1

--- src/2024/3/index.js
+++ src/2024/3/index.js
@@ -1,4 +1,4 @@
-import { input } from './input-example.js';
+import { input } from './input.js';
 
 export function main() {
   const data = getInputData(input);
@@ -7,13 +7,13 @@ export function main() {
   const part2Answer = part2(data);
 
   return `
-        <p><span class="answer">${part1Answer}</span></p>        
+        <p><span class="answer">Sum of all multiplications is ${part1Answer}</span>.</p>        
         <p><span class="answer">${part2Answer}</span></p>        
     `;
 }
 
 export function part1(data) {
-  return '';
+  return data.reduce((sum, pair) => sum + pair[0] * pair[1], 0);
 }
 
 export function part2(data) {
@@ -21,5 +21,7 @@ export function part2(data) {
 }
 
 export function getInputData(data) {
-  return data;
+  const instructions = data.match(/mul\(\d+,\d+\)/g);
+
+  return instructions.map(instruction => instruction.match(/\d+/g).map(el => +el));
 }
--- src/2024/3/index.test.js
+++ src/2024/3/index.test.js
@@ -1,11 +1,11 @@
 import { part1, part2, getInputData } from './index.js';
 import { input } from './input-example.js';
 
-describe('2024-%DAY%', () => {
+describe('2024-3', () => {
   const data = getInputData(input);
 
-  test('part 1: %TEST_NAME%', () => {
-    // expect(part1(data)).toBe();
+  test('part 1: should calculate the sum of all multiplications', () => {
+    expect(part1(data)).toBe(161);
   });
 
   test('part 2: %TEST_NAME%', () => {
