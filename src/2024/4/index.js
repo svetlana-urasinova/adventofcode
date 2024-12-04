98081742e44e301df3f15b28bd0ac669ea357175
svetlana-urasinova <urasinova.work@gmail.com>
2024-12-04 10:43:56 +0100

    Solve part 1

--- src/2024/4/index.js
+++ src/2024/4/index.js
@@ -1,4 +1,17 @@
-import { input } from './input-example.js';
+import { input } from './input.js';
+
+const SEARCH = 'XMAS';
+
+const DIRECTIONS = {
+  Top: 'top',
+  Bottom: 'bottom',
+  Left: 'left',
+  LeftTop: 'left-top',
+  LeftBottom: 'left-bottom',
+  Right: 'right',
+  RightTop: 'right-top',
+  RightBottom: 'right-bottom',
+};
 
 export function main() {
   const data = getInputData(input);
@@ -7,13 +20,21 @@ export function main() {
   const part2Answer = part2(data);
 
   return `
-        <p><span class="answer">${part1Answer}</span></p>        
+        <p>Word &laquo;XMAS&raquo; appears <span class="answer">${part1Answer}</span> times.</p>        
         <p><span class="answer">${part2Answer}</span></p>        
     `;
 }
 
 export function part1(data) {
-  return '';
+  let total = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      total += checkAllDirections(data, i, j).length;
+    }
+  }
+
+  return total;
 }
 
 export function part2(data) {
@@ -21,5 +42,66 @@ export function part2(data) {
 }
 
 export function getInputData(data) {
-  return data;
+  return data.split('\n').map(line => line.split(''));
+}
+
+function checkAllDirections(matrix, row, column) {
+  return Object.values(DIRECTIONS).filter(direction => checkDirection(matrix, row, column, direction));
+}
+
+function checkDirection(matrix, row, column, direction) {
+  let str = matrix[row]?.[column];
+
+  if (!str || !SEARCH.startsWith(str)) {
+    return false;
+  }
+
+  let currentRow = row;
+  let currentColumn = column;
+
+  for (let i = 1; i < SEARCH.length; i++) {
+    const neighborCoordinates = getNeighborCoordinates(currentRow, currentColumn, direction);
+
+    const currentChar = getChar(matrix, neighborCoordinates);
+
+    if (!currentChar) {
+      return false;
+    }
+
+    str += currentChar;
+
+    if (!SEARCH.startsWith(str)) {
+      return false;
+    }
+
+    currentRow = neighborCoordinates.row;
+    currentColumn = neighborCoordinates.column;
+  }
+
+  return str === SEARCH;
+}
+
+function getNeighborCoordinates(row, column, direction) {
+  switch (direction) {
+    case DIRECTIONS.Top:
+      return { row: [row - 1], column };
+    case DIRECTIONS.Bottom:
+      return { row: row + 1, column };
+    case DIRECTIONS.Left:
+      return { row, column: column - 1 };
+    case DIRECTIONS.LeftTop:
+      return { row: row - 1, column: column - 1 };
+    case DIRECTIONS.LeftBottom:
+      return { row: row + 1, column: column - 1 };
+    case DIRECTIONS.Right:
+      return { row: row, column: column + 1 };
+    case DIRECTIONS.RightTop:
+      return { row: row - 1, column: column + 1 };
+    case DIRECTIONS.RightBottom:
+      return { row: row + 1, column: column + 1 };
+  }
+}
+
+function getChar(matrix, coordinates) {
+  return matrix[coordinates.row]?.[coordinates.column];
 }
--- src/2024/4/index.test.js
+++ src/2024/4/index.test.js
@@ -4,8 +4,8 @@ import { input } from './input-example.js';
 describe('2024-4', () => {
   const data = getInputData(input);
 
-  test('part 1: %TEST_NAME%', () => {
-    // expect(part1(data)).toBe();
+  test('part 1: should calculate how many times XMAS appears', () => {
+    expect(part1(data)).toBe(18);
   });
 
   test('part 2: %TEST_NAME%', () => {
