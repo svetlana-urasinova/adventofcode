886b7e4437433c75c7e86c934a6085a8321c7c3b
svetlana-urasinova <urasinova.work@gmail.com>
2024-12-04 15:48:15 +0100

    Create utils for easier visualization

---
+++ src/vizualisation/colors/colors.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  White: 'white',
+  Red: 'red',
+  Green: 'green',
+  Blue: 'blue',
+  Yellow: 'yellow',
+  Purple: 'purple',
+  LightBlue: 'light-blue',
+  Orange: 'orange',
+};
---
+++ src/vizualisation/display/display.js
@@ -0,0 +1,39 @@
+export class Display {
+  _contentElement;
+
+  constructor(selector) {
+    this._contentElement = document.querySelector(selector)?.querySelector('.display-content');
+
+    if (!this._contentElement) {
+      throw new Error(`No display element for selector "${selector}" is found.`);
+    }
+  }
+
+  updateElement(content, selector) {
+    const element = this._getElement(selector);
+
+    element.innerHTML = '';
+
+    element.append(content);
+  }
+
+  clearElement(selector) {
+    const element = this._getElement(selector);
+
+    element.innerHTML = '';
+  }
+
+  getElementContent(selector) {
+    return this._getElement(selector).textContent;
+  }
+
+  _getElement(selector) {
+    const element = selector ? this._contentElement.querySelector(selector) : this._contentElement;
+
+    if (selector && !element) {
+      throw new Error(`Cannot find element for selector ${selector}`);
+    }
+
+    return element;
+  }
+}
