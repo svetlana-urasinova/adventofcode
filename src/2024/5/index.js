1a4c1c3b498d4040c004bb2f230dccf9f07c221a
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2024-12-05 13:29:26 +0100

    Solve part 1

--- src/2024/5/index.js
+++ src/2024/5/index.js
@@ -13,7 +13,9 @@ export function main() {
 }
 
 export function part1(data) {
-  return '(answer)';
+  const { rules, updates } = data;
+
+  return updates.filter(update => isUpdateValid(update, rules)).reduce((sum, update) => sum + getMiddle(update), 0);
 }
 
 export function part2(data) {
@@ -21,5 +23,37 @@ export function part2(data) {
 }
 
 export function getInputData(data) {
-  return data;
+  const [rulesData, updatesData] = data.split('\n\n');
+
+  const rules = rulesData.split('\n').reduce((acc, line) => {
+    const [first, second] = line.split('|');
+
+    acc[first] = [...(acc[first] || []), +second];
+
+    return acc;
+  }, {});
+
+  const updates = updatesData.split('\n').map(line => line.split(',').map(number => +number));
+
+  return { rules, updates };
+}
+
+function isUpdateValid(update, rules) {
+  const sortedNumbers = sortNumbers(update, rules);
+
+  return update.every((number, index) => sortedNumbers[index] === number);
+}
+
+function sortNumbers(numbers, rules) {
+  return [...numbers].sort(
+    (a, b) => getFilteredRule(numbers, rules[b]).length - getFilteredRule(numbers, rules[a]).length
+  );
+}
+
+function getFilteredRule(numbers, rule) {
+  return rule?.filter(numberAfter => numbers.includes(numberAfter)) || [];
+}
+
+function getMiddle(numbers) {
+  return numbers[Math.floor(numbers.length / 2)];
 }
