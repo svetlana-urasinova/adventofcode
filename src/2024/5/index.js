5b4d3f2b62c54a87ffde19cf1a44e1b62693bf04
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2024-12-05 11:17:16 +0100

    Setup 2024-5

---
+++ src/2024/5/index.js
@@ -0,0 +1,25 @@
+import { input } from './input-example.js';
+
+export function main() {
+  const data = getInputData(input);
+
+  const part1Answer = part1(data);
+  const part2Answer = part2(data);
+
+  return `
+        <p>Answer to part 1: <span class="answer">${part1Answer}</span></p>        
+        <p>Answer to part 2: <span class="answer">${part2Answer}</span></p>        
+    `;
+}
+
+export function part1(data) {
+  return '(answer)';
+}
+
+export function part2(data) {
+  return '(answer)';
+}
+
+export function getInputData(data) {
+  return data;
+}
