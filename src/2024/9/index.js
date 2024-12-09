f2eb7cde77134602800a1e8be2ff2864bc1484d8
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2024-12-09 13:58:33 +0100

    Solve part 2

--- src/2024/9/index.js
+++ src/2024/9/index.js
@@ -1,100 +1,117 @@
 import { EMPTY } from '../../constants/empty.js';
-import { input } from './input-example.js';
+import { input } from './input.js';
 
 export function main() {
-  const data = getInputData(input);
-
-  const part1Answer = part1(data);
-  // const part1Answer = part1(getInputData('12345'));
-  const part2Answer = part2(data);
+  const part1Answer = part1(input);
+  const part2Answer = part2(input);
 
   return `
-        <p>The resulting filesystem checksum is <span class="answer">${part1Answer}</span>.</p>        
-        <p>Answer to part 2: <span class="answer">${part2Answer}</span>.</p>        
+        <p>The resulting filesystem checksum with splitting files is <span class="answer">${part1Answer}</span>.</p>        
+        <p>The resulting filesystem checksum without splitting files is <span class="answer">${part2Answer}</span>.</p>        
     `;
 }
 
-export function part1(data) {
-  removeEmptyFromRight(data);
+export function part1(input) {
+  const data = getInputData(input);
 
   let i = 0;
+  let j = data.length - 1;
 
-  while (i < data.length) {
+  while (i < j) {
     if (data[i].value === EMPTY) {
-      const update = generateSplitFileUpdate(i, data);
+      while (data[i].freeSpace > 0) {
+        while (data[j].value === EMPTY || data[j].length === 0) {
+          j--;
+        }
 
-      data.splice(i, 1, ...update);
+        if (i >= j) {
+          break;
+        }
 
-      i += update.length;
-    } else {
-      i++;
+        if (data[i].freeSpace < data[j].length) {
+          data[i].children.push({ value: data[j].value, length: data[i].freeSpace });
+          data[j].length -= data[i].freeSpace;
+          data[i].freeSpace = 0;
+        } else {
+          data[i].children.push({ value: data[j].value, length: data[j].length });
+          data[i].freeSpace -= data[j].length;
+          removeElement(j, data);
+        }
+      }
+    }
+
+    i++;
+  }
+
+  return calculateCheckSum(data);
+}
+
+export function part2(input) {
+  const data = getInputData(input);
+
+  const files = data.map((el, index) => ({ ...el, index })).filter(data => data.value !== EMPTY);
+
+  for (let i = files.length - 1; i >= 0; i--) {
+    const { value, length, index } = files[i];
+
+    for (let j = 0; j < index; j++) {
+      if (data[j].value === EMPTY && data[j].freeSpace >= length) {
+        data[j].children.push({ value, length });
+        data[j].freeSpace -= length;
+
+        removeElement(index, data);
+
+        break;
+      }
     }
   }
 
   return calculateCheckSum(data);
 }
 
-export function part2(data) {
-  return '(answer)';
-}
-
 export function getInputData(input) {
-  return input
-    .split('')
-    .reduce((result, char, index) => [...result, { value: index % 2 === 0 ? index / 2 : EMPTY, length: +char }], []);
+  return input.split('').reduce((result, char, index) => {
+    const isEven = index % 2 === 0;
+
+    const element = isEven
+      ? { value: index / 2, length: +char }
+      : { value: EMPTY, length: +char, freeSpace: +char, children: [] };
+
+    return [...result, element];
+  }, []);
 }
 
 function calculateCheckSum(data) {
+  let total = 0;
   let index = 0;
 
-  return data.reduce((sum, el) => {
-    const { value, length } = el;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].value === EMPTY) {
+      data[i].children.forEach(child => {
+        total += calculateCheckSumForElement(child, index);
 
-    for (let i = 0; i < length; i++) {
-      sum += value * index;
-      index++;
+        index += child.length;
+      });
+
+      if (data[i].freeSpace > 0) {
+        index += data[i].freeSpace;
+      }
+    } else {
+      total += calculateCheckSumForElement(data[i], index);
+
+      index += data[i].length;
     }
-
-    return sum;
-  }, 0);
-}
-
-function removeEmptyFromRight(data) {
-  while (data[data.length - 1].value === EMPTY || data[data.length - 1].length === 0) {
-    data.pop();
   }
 
-  return data;
+  return total;
 }
 
-function generateSplitFileUpdate(index, data) {
-  const update = [];
+function calculateCheckSumForElement(element, index) {
+  const { value, length } = element;
 
-  let length = data[index].length;
-
-  while (length > 0) {
-    removeEmptyFromRight(data);
-
-    const lastElement = data.at(-1);
-
-    const currentUpdateLength = Math.min(length, lastElement.length);
-
-    update.push({ value: lastElement.value, length: currentUpdateLength });
-
-    length -= currentUpdateLength;
-
-    data.at(-1).length -= currentUpdateLength;
-  }
-
-  return update;
+  return ((2 * index + length - 1) * length * value) / 2;
 }
 
-function printData(data) {
-  console.log(
-    data.reduce((acc, el) => {
-      const { value, length } = el;
-
-      return acc + value.toString().repeat(length);
-    }, '')
-  );
+function removeElement(index, data) {
+  data[index] = { value: EMPTY, length: data[index].length, freeSpace: data[index].length, children: [] };
 }
--- src/2024/9/index.test.js
+++ src/2024/9/index.test.js
@@ -2,13 +2,11 @@ import { part1, part2, getInputData } from './index.js';
 import { input } from './input-example.js';
 
 describe('2024-9', () => {
-  const data = getInputData(input);
-
-  test('part 1: should calculate the resulting filesystem checksum', () => {
-    expect(part1(data)).toBe(1928);
+  test('part 1: should calculate the resulting filesystem checksum with splitting files', () => {
+    expect(part1(input)).toBe(1928);
   });
 
-  test('part 2: %TEST_NAME%', () => {
-    // expect(part2(data)).toBe();
+  test('part 2: should calculate the resulting filesystem checksum without splitting files', () => {
+    expect(part2(input)).toBe(2858);
   });
 });
