6064685c0b2aac61c6e753ee3db28c57a4cb29f4
Svetlana Urasinova <svetlana.urasinova@ottonova.de>
2024-12-11 07:32:26 +0100

    Setup

---
+++ src/2024/11/index.test.js
@@ -0,0 +1,14 @@
+import { part1, part2, getInputData } from './index.js';
+import { input } from './input-example.js';
+
+describe('2024-11', () => {
+  const data = getInputData(input);
+
+  test('part 1: %TEST_NAME%', () => {
+    // expect(part1(data)).toBe();
+  });
+
+  test('part 2: %TEST_NAME%', () => {
+    // expect(part2(data)).toBe();
+  });
+});
---
+++ src/2024/11/input-example.js
@@ -0,0 +1,1 @@
+export const input = `0 1 10 99 999`;
