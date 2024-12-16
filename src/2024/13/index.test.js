3dcf67e79f86410280f4a5dad6c3713f40c48ce2
Svetlana Urasinova <54736670+svetlana-urasinova@users.noreply.github.com>
2024-12-16 17:01:40 +0100

    2024 13 (#33)
    Co-authored-by: Svetlana Urasinova <svetlana.urasinova@ottonova.de>

---
+++ src/2024/13/index.test.js
@@ -0,0 +1,14 @@
+import { part1, part2, getInputData } from './index.js';
+import { input } from './input-example.js';
+
+describe('2024-%DAY%', () => {
+  const data = getInputData(input);
+
+  test('part 1: should calculate the fewest tokens you would have to spend to win all possible prizes', () => {
+    // expect(part1(data)).toBe();
+  });
+
+  test('part 2: %TEST_NAME%', () => {
+    // expect(part2(data)).toBe();
+  });
+});
